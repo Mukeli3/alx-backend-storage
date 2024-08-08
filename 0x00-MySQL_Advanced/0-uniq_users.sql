93c8db1333031159afd666643e543644fd6ad10c
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-08 18:19:42 +0000

    uniq

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- script creates a table, users and does not fail if table already exists
+CREATE TABLE IF NOT EXISTS users(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+	);
