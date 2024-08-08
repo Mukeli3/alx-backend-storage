c3565c15ac9c12df155e11536f06793e71dfc8b4
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-08 22:30:58 +0000

    fixed length

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
--- SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
--- mysql -u root -p database_name < path_to_metal_bands.sql (import table dump)
+-- SQL script that ranks number of (non-unique) fans
+-- mysql -u root -p database_name < metal_bandspath.sql
 SELECT origin, SUM(total_fans) AS nb_fans
 FROM metal_band GROUP BY origin ORDER BY nb_fans DESC;
