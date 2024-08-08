ee831fc4f08af84903030d7c3eab111d8988555d
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-08 22:35:32 +0000

    removed new line

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,3 @@
 -- SQL script that ranks number of (non-unique) fans
 -- mysql -u root -p database_name < metal_bandspath.sql
-SELECT origin, SUM(total_fans) AS nb_fans
-FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
+SELECT origin, SUM(total_fans) AS nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
