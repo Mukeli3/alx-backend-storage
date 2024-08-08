708a61383148e706fc4857b38a8dc7faf49b6168
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-08 21:59:30 +0000

    few fixes

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -3,8 +3,8 @@ DELIMITER $$
 CREATE TRIGGER reset_mail
 BEFORE UPDATE ON users FOR EACH ROW
 BEGIN
-    IF NEW.email <> OLD.EMAIL THEN
-	SET NEW.valid_email = FALSE
+    IF NEW.email <> OLD.email THEN
+	SET NEW.valid_email = FALSE;
     END IF;
 END $$
 DELIMITER ;
