f8b524e12a057f735c6a3c1f356d7e7b94d53f3d
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-08 19:07:35 +0000

    reset

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+-- SQL script creates a trigger that resets attributes
+DELIMITER $$
+CREATE TRIGGER reset_mail
+BEFORE UPDATE ON users FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.EMAIL THEN
+	SET NEW.valid_email = FALSE
+    END IF;
+END $$
+DELIMITER ;
