55f77d09eba899d04a120c730544908c05cb05a0
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-13 22:34:10 +0000

    documented

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -6,5 +6,11 @@ import pymongo
 
 
 def list_all(mongo_collection):
+    """ lists all documents in a collection
+    Args:
+       mongo_collection: The PyMongo collection obj
+    Returns:
+       A list of docs or an empty list if no docs are found
+    """
     docs = list(mongo_collection.find())
     return docs
