7474f039d65df334c7721fb00753a429f01a8915
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-13 22:39:46 +0000

    function documentation added

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,6 +6,15 @@ based on the name
 
 
 def update_topics(mongo_collection, name, topics):
+    """Updates topics for a school document based on the name.
+    Args:
+        mongo_collection: The PyMongo collection object.
+        name: The school name to update.
+       topics:  
+       A list of topics to be updated.
+    Returns:
+       The number of documents updated.
+  """
     mongo_collection.update_many(
             {"name": name},
             {"$set": {"topics": topics}}
