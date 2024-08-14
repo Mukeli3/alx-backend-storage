1427c021474e8c6f9ca98da8ffbcb43c955abb18
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-14 21:32:10 +0000

    corrected output

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -15,13 +15,13 @@ class Cache:
         """
         method stores an instance of the Redis client as a private variable
         """
-        self.redis = redis.Redis()
+        self._redis = redis.Redis()
         self._redis.flushdb()  # clear the Redis database
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         method takes an argument and returns a string, generates a random key
         """
-        key = str(uui.uuid4())  # generate a random key
+        key = str(uuid.uuid4())  # generate a random key
         self._redis.set(key, data)  # store data in Redis
         return key  # return key
