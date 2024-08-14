3338426c4f59f01ab9d5b653e9795a1aaf2f7df6
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-14 21:24:02 +0000

    commented

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """
+This module defines a class Cache
 """
 import redis
 import uuid
@@ -7,11 +8,20 @@ from typing import Union
 
 
 class Cache:
+    """
+    Cache class
+    """
     def __init__(self):
+        """
+        method stores an instance of the Redis client as a private variable
+        """
         self.redis = redis.Redis()
         self._redis.flushdb()  # clear the Redis database
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        method takes an argument and returns a string, generates a random key
+        """
         key = str(uui.uuid4())  # generate a random key
         self._redis.set(key, data)  # store data in Redis
         return key  # return key
