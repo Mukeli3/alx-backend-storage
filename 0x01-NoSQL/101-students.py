2ce787b01eb576f1e0739797c19fe4a3008e5c0c
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-14 00:09:46 +0000

    average score

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+This module defines a function that returns all studens sorted by
+average score
+"""
+from pymongo import MongoClient
+
+
+def top_students(mongo_collection):
+    """
+    Returns all students sorted by their average score.
+
+    Args:
+        mongo_collection: pymongo collection object.
+
+    Returns:
+        A list of students sorted by average score with each student having
+        an additional field 'averageScore'.
+    """
+    # Aggregation pipeline
+    pipel = [
+            {
+                "$project": {
+                    "name": 1,  # Include the name field
+                    "averageScore": { "$avg": "$scores" }
+                    }
+                },
+            {
+                "$sort": { "averageScore": -1 }  # Sort by average score in
+                # descending order
+                }
+            ]
+
+    # Execute the aggregation pipeline
+    return list(mongo_collection.aggregate(pipel))
