df8de71bada4c6ac6ff6e40beabccf0d6d44c59b
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-14 21:46:52 +0000

    get

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ This module defines a class Cache
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -25,3 +25,22 @@ class Cache:
         key = str(uuid.uuid4())  # generate a random key
         self._redis.set(key, data)  # store data in Redis
         return key  # return key
+
+    def get(self, key: str, fn: Optional[Callable] = None)
+    -> Union[str, bytes, int, None]:
+        """
+        method takes a key str arg and an optional Callable arg used o convert
+        the data back to the desired format
+        """
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """ get a str value from Redis by key """
+        return self.get(key, lambda d: d.decode('utf-8'))
+
+    def get_int(self, key:str) -> Optional[int]:
+        """ get an int value from Redis by key """
+        return self.get(key, lambda x: int(x))
